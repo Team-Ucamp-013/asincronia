4edb18215534cd376acd95bcb9d3ef6b824c3f04
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-01-14 12:51:10 -0600

    get data asyn/await

--- index.js
+++ index.js
@@ -20,40 +20,72 @@ console.log('holi')
 // saludar('Dany')
 
 
-const saludar = new Promise((resolve, reject) => {
-  // if(nombre === 'pedro') {
-  //   resolve('Se resuelve la promesa, Holi')
-  // } else {
-  //   reject('Ha fallado la promesa')
-  // }
-  setTimeout(() => {
-    resolve('Se resuelve la promesa, Holi')
-    reject('Ha fallado la promesa')
-  }, 4000);
-})
+// const saludar = new Promise((resolve, reject) => {
+//   // if(nombre === 'pedro') {
+//   //   resolve('Se resuelve la promesa, Holi')
+//   // } else {
+//   //   reject('Ha fallado la promesa')
+//   // }
+//   setTimeout(() => {
+//     resolve('Se resuelve la promesa, Holi')
+//     reject('Ha fallado la promesa')
+//   }, 4000);
+// })
 
-saludar
-  .then((resultado) => {
-    console.log(resultado)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-  .finally(() => console.log('final'))
-
-
-const status = new Promise((resolve, reject) => {
-  const valor = 5
-  if (valor >= 5) {
-    resolve('Es mayor a 5')
-  } else {
-    reject('Es menor a 5')
-  }
-})
-
-status
-  .then((resultado) => console.log(resultado))
-  .catch((error) => console.log(error))
+// saludar
+//   .then((resultado) => {
+//     console.log(resultado)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+//   .finally(() => console.log('final'))
 
 
+// const status = new Promise((resolve, reject) => {
+//   const valor = 5
+//   if (valor >= 5) {
+//     resolve('Es mayor a 5')
+//   } else {
+//     reject('Es menor a 5')
+//   }
+// })
 
+// status
+//   .then((resultado) => console.log(resultado))
+//   .catch((error) => console.log(error))
+
+let containerList = document.getElementById('list')
+console.log(containerList)
+const render = (data) => {
+  data.forEach(element => {
+    containerList.innerHTML += `
+      <div>
+        <img src="${element.image}" />
+        <h2>
+          ${element.name}
+        </h2>
+      </div>
+    `
+
+  });
+}
+
+// const getData = async () => {
+//   const response = await fetch('https://rickandmortyapi.com/api/character')
+//   const characters = await response.json()
+//   console.log(characters.results)
+//   render(characters.results)
+// }
+const getData = () => {
+  fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(result => {
+      console.log(result)
+      render(result.results)
+    })
+  // const characters =  response.json()
+  // console.log(characters.results)
+}
+
+getData()
